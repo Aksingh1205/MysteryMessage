71403ba046e3f5d05b9ded31c5f05e8aff993f9a
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2024-11-15 10:08:03 +0530

    Defined routes for sending and receiving messages

--- src/app/api/accept-message/route.ts
+++ src/app/api/accept-message/route.ts
@@ -18,6 +18,73 @@ export async function POST(request : Request){
     }
 
     const userID = user._id
+    const {acceptMessages} = await request.json()
+
+    try {
+        const updatedUser = await UserModel.findByIdAndUpdate(
+            userID,
+            {isAcceptingMessages : acceptMessages},
+            {new : true}
+        )
+
+        if(!updatedUser){
+            return Response.json({
+                success : false,
+                message : "failed to update user status to accept messages"
+            }, {status : 401})  
+        }
+        return Response.json({
+            success : true,
+            message : "Message acceptance status updated successfully",
+            updatedUser
+        }, {status : 200})  
+    } catch (error) {
+        console.log('failed to update user status to accept messages');
+        return Response.json({
+            success : false,
+            message : "failed to update user status to accept messages",
+            error
+        }, {status : 500})
+        
+    }
 
 
+}
+
+export async function GET(request : Request){
+    await dbConnect()
+
+    const session = await auth()
+    const user : User = session?.user
+
+    if(!session || !session.user){
+        return Response.json({
+            success : false,
+            message : "Not Authenticated"
+        }, {status : 401})
+    }
+
+    const userID = user._id
+    
+    try {
+        const foundUser = await UserModel.findById(userID)
+    if(!foundUser){
+        return Response.json({
+            success : false,
+            message : "User not found"
+        }, {status : 404})  
+    }
+
+    return Response.json({
+        success : true,
+        isAcceptingMessages : foundUser.isAcceptingMessages
+    }, {status : 200}) 
+    } catch (error) {
+        console.log('failed to update user status to accept messages');
+        return Response.json({
+            success : false,
+            message : "Error in getting message acceptance status",
+            error
+        }, {status : 500})
+    }
 }
